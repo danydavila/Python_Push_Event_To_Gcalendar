c9b7f4f6f6c51a0d5b1fc7f3d77ef03b57273b2b
Dany Davila <danydavila@gmail.com>
2024-09-09 07:20:11 -0400

    Adding project structure

---
+++ src/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Prevents main() from being executed during imports.
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
